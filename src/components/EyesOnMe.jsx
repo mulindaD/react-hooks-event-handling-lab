b286a00f26a51190fb282dc51b84cbb1261f7af9
Dominic Mulinda <mulindarweshumba@gmail.com>
2024-08-23 09:53:12 +0300

    Events handling lab

--- src/components/EyesOnMe.jsx
+++ src/components/EyesOnMe.jsx
@@ -9,9 +9,9 @@ function EyesOnMe(){
         console.log('Hey! Eyes on me!')
     }
     return(
-        <div>
+        <>
             <button onFocus={handleOnFocus} onBlur={handleOnBlur}>Eyes on me</button>
-        </div>
+        </>
     )
 }
 
